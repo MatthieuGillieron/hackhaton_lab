a99aae3546b8c16c91fccc1a3c53b20f8b2af8fa
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-10-11 15:04:04 +0200

    add: Practice exercice widget + logic for (dev-backend)

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -59,7 +59,7 @@ if prompt := st.chat_input("Écrivez votre message ici..."):
     
     # setting payload
     payload = {
-        "model": "llama3",
+        "model": "qwen3",
         "messages": st.session_state.messages,
         "temperature": 0.7,
         "max_tokens": 1000
