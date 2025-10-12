4d7a2679ff316fc1835387202346ac0a2eb9f82e
Mattias Dürrmeier <matt.duerr@pm.me>
2025-10-12 13:17:37 +0200

    Update vector store script with gemma-2

--- notebooks/create_vector_store.py
+++ notebooks/create_vector_store.py
@@ -5,6 +5,13 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import numpy as np
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PRODUCT_ID = os.getenv("PRODUCT_ID")
+API_TOKEN = os.getenv("API_TOKEN")
 
 
 DB_PATH = "./data/"
@@ -13,7 +20,29 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 }
 
-MODEL_NAME = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+MODEL_NAME = "bge_multilingual_gemma2"
+
+class MultinligualGemma2(EmbeddingFunction):
+    def __init__(self, model_name: str) -> None:
+        self.model_name = model_name
+        self.url = f"https://api.infomaniak.com/1/ai/{PRODUCT_ID}/openai/v1/embeddings"
+        self.headers = {
+          'Authorization': f"Bearer {API_TOKEN}",
+          'Content-Type': 'application/json',
+        }
+
+    def __call__(self, input_data: Documents) -> Embeddings:
+        payload = {
+            "input": input_data,
+            "model": self.model_name,
+        }
+
+        req = requests.post(url=self.url, json=payload, headers=self.headers)
+        res = req.json()
+        data = res["data"]
+        embeddings = [np.array(x["embedding"]) for x in data]
+
+        return embeddings
 
 
 class SentenceTransformerFunction(EmbeddingFunction):
@@ -51,9 +80,10 @@ metadatas: list[dict[str, str]] = df.loc[:, ["title", "type", "url"]].to_dict("r
 
 # create and add to vector store
 collection = client.create_collection(
-    name="main",
-    embedding_function=SentenceTransformerFunction(MODEL_NAME),
+    name="main-gemma",
+    embedding_function=MultinligualGemma2(MODEL_NAME),
 )
+print("Name:", collection)
 
 print("Embedding and adding the documents... ")
 collection.add(
